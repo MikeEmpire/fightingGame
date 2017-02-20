270e310ae16151d63c28f5182bb595c4fc4b09f6
Mike <aolie94@gmail.com>
2017-02-20 03:38:55 -0800

    Added images, and placed them on html relative
    Images are put in place and ready for functionality

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var characters = {
+    playerCharacter: [{ "name": "Cloud", "hp": 250, "description": "From Somewhere" }],
+    enemies: {
+        jelly: [{ "name": "Slime", "hp": 200, "description": "Just a damn slime" }],
+        reaper: [{ "name": "Reaper", "hp": 200, "description": "Just a damn Reaper" }],
+        griffin: [{ "name": "Griffin", "hp": 200, "description": "Just a bird" }]
+    }
+};
